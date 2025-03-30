5a284a753e407cd02abb23fc31a3ffe690577225
Israel Nicacio <israel.nicacio@outlook.com>
2025-03-30 19:59:49 -0300

    Adicionado pacotes e finalizado persistencia de dados

---
+++ src/Cadastro/DZA.Cadastro.Infrastructure.Data/Mappings/PessoaMapping.cs
@@ -0,0 +1,31 @@
+﻿using DZA.Cadastro.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DZA.Cadastro.Infrastructure.Data.Mappings;
+
+internal class PessoaMapping : IEntityTypeConfiguration<Pessoa>
+{
+    public void Configure(EntityTypeBuilder<Pessoa> builder)
+    {
+        builder.HasKey(k => k.Id);
+
+        builder.Property(p => p.Nome)
+            .IsRequired()
+            .HasColumnType("varchar(60)");
+
+        builder.Property(p => p.SobreNome)
+            .IsRequired()
+            .HasColumnType("varchar(60)");
+
+        builder.Property(p => p.Idade)
+            .IsRequired()
+            .HasColumnType("integer");
+
+        builder.HasOne(o => o.Departamento)
+            .WithMany(m => m.Pessoas)
+            .HasForeignKey(fk => fk.DepartamentoId);
+
+        builder.ToTable("Pessoas");
+    }
+}
---
+++ src/Cadastro/DZA.Cadastro.Infrastructure.Data/Repositories/PessoaRepository.cs
@@ -0,0 +1,50 @@
+﻿using DZA.Cadastro.Domain.Entities;
+using DZA.Cadastro.Domain.Repositories;
+using DZA.Cadastro.Infrastructure.Data.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace DZA.Cadastro.Infrastructure.Data.Repositories;
+
+public class PessoaRepository : IPessoaRepository
+{
+    private readonly CadastroContext _cadastroContext;
+
+    public PessoaRepository(CadastroContext cadastroContext) 
+        => _cadastroContext = cadastroContext;
+
+    public async Task<IEnumerable<Pessoa>> ObterPessoas() 
+        => await _cadastroContext.Pessoas.ToListAsync();
+
+    public async Task<Pessoa?> ObterPessoaPorId(Guid id)
+        => await _cadastroContext.Pessoas.FindAsync(id);
+
+    public async Task<IEnumerable<Pessoa>?> ObterPessoasPorIdDepartamento(Guid idDepartamento)
+    {
+        var dep = await _cadastroContext.Departamentos
+        .Where(f => f.Id == idDepartamento)
+        .FirstOrDefaultAsync();
+
+        return dep?.Pessoas;
+    }
+
+    public void AdicionarPessoa(Pessoa pessoa) 
+        => _cadastroContext.Pessoas.Add(pessoa);
+
+    public void AtualizarPessoa(Pessoa pessoa)
+        => _cadastroContext.Update(pessoa);
+
+
+    public async Task<IEnumerable<Departamento>> ObterDepartamentos() 
+        => await _cadastroContext.Departamentos.ToListAsync();
+
+    public async Task<Departamento?> ObterDepartamentoPorId(Guid id)
+        => await _cadastroContext.Departamentos.FindAsync(id);
+
+    public void AdicionarDepartamento(Departamento departamento) 
+        => _cadastroContext.Departamentos.Add(departamento);
+
+    public void AtualizarDepartamento(Departamento departamento) 
+        => _cadastroContext.Update(departamento);
+
+    public void Dispose() => GC.SuppressFinalize(this);
+}
