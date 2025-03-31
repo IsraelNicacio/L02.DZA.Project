5bf7854df9577bdc603a3d241772de6daa598a6a
Israel Nicacio <israel.nicacio@outlook.com>
2025-03-31 08:21:45 -0300

    Estava faltando o UnitOfWork para persistencia de dados

---
+++ src/Cadastro/DZA.Cadastro.Infrastructure.Data/Repositories/UnitOfWork.cs
@@ -0,0 +1,22 @@
+﻿using DZA.Cadastro.Domain.Repositories;
+using DZA.Cadastro.Infrastructure.Data.Context;
+
+namespace DZA.Cadastro.Infrastructure.Data.Repositories;
+
+public class UnitOfWork : IUnitOfWork
+{
+    private readonly CadastroContext _cadastroContext;
+    private IPessoaRepository _pessoa;
+
+    public UnitOfWork(CadastroContext cadastroContext) 
+        => _cadastroContext = cadastroContext;
+
+    public IPessoaRepository Pessoa 
+        => _pessoa ?? (_pessoa = new PessoaRepository(_cadastroContext));
+
+    public async Task<bool> Commit() 
+        => await _cadastroContext.SaveChangesAsync() > 0;
+
+    public void Dispose() 
+        => _cadastroContext.DisposeAsync();
+}
